f66dba0dab5c4fb989e2948f099cc49a3b1da962
秋鸣 <44081667+QiuMingZS@users.noreply.github.com>
2018-10-12 20:44:45 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,6 +5,6 @@ int main()
 	// 从硬盘中读取图像文件
 	Mat srcImage = imread("F:\\VS17_OpenCV3\\Material\\LongMao.jpg");
 	imshow("The Image", srcImage);			// 显示图像
-	waitKey(0);								// 等待任意键按下
+	waitKey(0);					// 等待任意键按下
 	return 0;
 }
